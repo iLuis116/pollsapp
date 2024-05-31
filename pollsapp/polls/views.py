6986abf680ab3cdebeeab71cb45b7280fc508328
Jose Luis <84213931+iLuis116@users.noreply.github.com>
2024-05-31 13:15:45 -0600

    Estilos

--- pollsapp/polls/views.py
+++ pollsapp/polls/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
@@ -13,15 +14,14 @@ def home(request):
     })
 
 def vote (request, q_id):
-    question= get_object_or_404(Question, id=q_id)
-
+    question = get_object_or_404(Question, id=q_id)
+    question = Question.objects.get(id=q_id)
     if request.method =='POST':
         try:
             choice = request.POST['choice']
             c = Choise.objects.get (id=choice)
             c.votes += 1
             c.save()
-
             return redirect('polls:results', q_id)
         except (KeyError):
             return render(request, 'polls/question.html',
@@ -31,9 +31,18 @@ def vote (request, q_id):
                   {"question": question })
 
 def results(request, q_id):
-     try:  
-        question= Question.objects.get(id=q_id)
+     try:
+         question= Question.objects.get(id=q_id)
      except Question.DoesNotExist:
-         raise Http404("Question does not exist")
-     return render(request,'polls/results.html',
-                  {"question": question })
+        raise Http404("Question does not exist")
+     
+     labels = [c.choise_text for c in question.choise_set.all()]
+     data = [c.votes for c in question.choise_set.all()]
+     context = {"question": question, "labels": labels, "data": data}
+     return render(request, "polls/results.html", context)
+
+    # return render(request,'polls/results.html',
+     #             {"question": question })
+                  
+
+
